a150cfb90aa091399a30f7ca55c2e40ea5c44c6f
itstruce <107202816+its-truce@users.noreply.github.com>
2023-02-10 21:05:21 +0530

    added information files

---
+++ main/info/nkapi.py
@@ -0,0 +1,70 @@
+def emotify(monkey):
+    if monkey == "DartMonkey":
+        monkey = "<:dartmonkey:1072795437272145951> Dart Monkey"
+        return monkey
+    elif monkey == "BoomerangMonkey":
+        monkey = "<:boomerangmonkey:1072795463335555193> Boomerang Monkey"
+        return monkey
+    elif monkey == "BombShooter":
+        monkey = "<:bombshooter:1072795501042352218> Bomb Shooter"
+        return monkey
+    elif monkey == "TackShooter":
+        monkey = "<:tack:1072795524438183936> Tack Shooter"  
+        return monkey  
+    elif monkey == "IceMonkey":
+        monkey = "<:ice:1072798079465554001> Ice Monkey" 
+        return monkey
+    elif monkey == "GlueGunner":
+        monkey = "<:glue:1072798211238023238> Glue Gunner"
+        return monkey
+    elif monkey == "SniperMonkey":
+        monkey = "<:sniper:1072798122331349022> Sniper Monkey"
+        return monkey
+    elif monkey == "MonkeySub":
+        monkey = "<:sub:1072798164446351362> Monkey Sub"  
+        return monkey  
+    elif monkey == "MonkeyBuccaneer":
+        monkey = "<:buccaneer:1072800281659723827> Monkey Buccaneer"
+        return monkey
+    elif monkey == "MonkeyAce":
+        monkey = "<:ace:1072800029657546822> Monkey Ace"
+        return monkey
+    elif monkey == "HeliPilot":
+        monkey = "<:heli:1072800439344562296> Heli Pilot"
+        return monkey
+    elif monkey == "MortarMonkey":
+        monkey = "<:mortar:1072799942927728660> Mortar Monkey"
+        return monkey
+    elif monkey == "DartlingGunner":
+        monkey = "<:dartling:1072800388060823612> Dartling Gunner"     
+        return monkey
+    elif monkey == "WizardMonkey":
+        monkey = "<:wizard:1072793911266906123> Wizard Monkey"
+        return monkey
+    elif monkey == "SuperMonkey":
+        monkey = "<:super:1072800321165873152> Super Monkey"
+        return monkey
+    elif monkey == "NinjaMonkey":
+        monkey = "<:ninjamonkey:1072799967015608350> Ninja Monkey"
+        return monkey
+    elif monkey == "Alchemist":
+        monkey = "<:alchemist:1072793861883174922> Alchemist"
+        return monkey
+    elif monkey == "Druid":
+        monkey = "<:druid:1072800351146754048> Druid"
+        return monkey
+    elif monkey == "BananaFarm":
+        monkey = "<:farm:1072800514124828722> Banana Farm"
+        return monkey
+    elif monkey == "SpikeFactory":
+        monkey = "<:spike:1072799915689902110> Spike Factory"
+        return monkey
+    elif monkey == "MonkeyVillage":
+        monkey = "<:village:1072800473003872338> Monkey Village"
+        return monkey
+    elif monkey == "EngineerMonkey":
+        monkey = "<:spike:1072799915689902110> Engineer Monkey"
+        return monkey
+
+# Test ID: 9fbc1ed8d8c2acf14e45dc180b21e775cc5618b499118b31
+# Test Challenge: ZMDSRCO
