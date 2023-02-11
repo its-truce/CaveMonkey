6e407342fa97a2cf7c35382cd0c489d1543d0900
itstruce <107202816+its-truce@users.noreply.github.com>
2023-02-11 19:29:33 +0530

    added new options to player command

--- main/extensions/player.py
+++ main/extensions/player.py
@@ -13,11 +13,11 @@ class PlayerDropdown(discord.ui.Select):
         self.color = color
         self.id = id
         options = [
-            discord.SelectOption(label="Main Info", description="Displays general information about the player."),
+            discord.SelectOption(label="Main Info", description="Displays general information about the player.", default=True),
             discord.SelectOption(label="Bloons Popped", description="Displays information about bloons popped by the player"),
             discord.SelectOption(label="Medals Singleplayer", description="Displays the player's singleplayer medals.")
         ]
-        super().__init__(placeholder="Choose what to display.",min_values=1, max_values=1, options=options)
+        super().__init__(min_values=1, max_values=1, options=options)
 
     async def callback(self, interaction: discord.Interaction):
         await interaction.response.defer()
@@ -31,6 +31,12 @@ class PlayerDropdown(discord.ui.Select):
             veteran = str(jsonr["body"]["veteranRank"])
             most_experienced = jsonr["body"]["mostExperiencedMonkey"]
             most_experienced = nkapi.emotify(most_experienced)
+            herolist = [jsonr["body"]["heroesPlaced"]["AdmiralBrickell"], jsonr["body"]["heroesPlaced"]["Adora"], jsonr["body"]["heroesPlaced"]["Benjamin"], 
+                jsonr["body"]["heroesPlaced"]["Etienne"], jsonr["body"]["heroesPlaced"]["Geraldo"], jsonr["body"]["heroesPlaced"]["Gwendolin"], 
+                jsonr["body"]["heroesPlaced"]["ObynGreenfoot"], jsonr["body"]["heroesPlaced"]["PatFusty"], jsonr["body"]["heroesPlaced"]["Psi"], 
+                jsonr["body"]["heroesPlaced"]["Quincy"], jsonr["body"]["heroesPlaced"]["Sauda"], jsonr["body"]["heroesPlaced"]["StrikerJones"]]
+            most_used_hero = herolist.index(max(herolist))
+            most_used_hero = nkapi.herofy(most_used_hero)
             highest_round = str(jsonr["body"]["highestRound"])
             achievements = str(jsonr["body"]["achievements"])
             followers = str(jsonr["body"]["followers"])
@@ -40,11 +46,15 @@ class PlayerDropdown(discord.ui.Select):
             player_embed.add_field(name="Rank", value=f"<:rank:1072789620883984414> {rank}", inline=False)
             player_embed.add_field(name="Veteran Rank", value=f"<:veteran:1072789996047704144> {veteran}", inline=False)
             player_embed.add_field(name="Most Experienced Monkey", value=most_experienced, inline=False)
+            player_embed.add_field(name="Most Used Hero", value=most_used_hero, inline=False)
             player_embed.add_field(name="Highest Round", value=f"<:red:1072864121088909322> {highest_round}", inline=False)
             player_embed.add_field(name="Followers", value=f"<:dartface:1072900168275468318> {followers}", inline=False)
             player_embed.add_field(name="Achievements", value=f"<:achievements:1073259163653787728> {achievements}", inline=False)
             player_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
-            await interaction.message.edit(embed=player_embed)
+            self.options[1].default = False
+            self.options[2].default = False
+            self.options[0].default = True
+            await interaction.edit_original_response(embed=player_embed, view=self.view)
         if self.values[0] == "Bloons Popped":
             uname = jsonr["body"]["displayName"]
             avatar = jsonr["body"]["avatarURL"]
@@ -69,8 +79,50 @@ class PlayerDropdown(discord.ui.Select):
             bloons_embed.add_field(name="ZOMGs Popped", value=f"<:zomg:1073604448665993376> {zomg}", inline=False)
             bloons_embed.add_field(name="BADs Popped", value=f"<:bad:1073604438876487860> {bad}", inline=False)
             bloons_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
-            await interaction.message.edit(embed=bloons_embed)
-
+            self.options[0].default = False
+            self.options[2].default = False
+            self.options[1].default = True
+            await interaction.edit_original_response(embed=bloons_embed, view=self.view)
+        if self.values[0] == "Medals Singleplayer":
+            uname = jsonr["body"]["displayName"]
+            avatar = jsonr["body"]["avatarURL"]
+            easy = str(jsonr["body"]["_medalsSinglePlayer"]["Easy"])
+            medium = str(jsonr["body"]["_medalsSinglePlayer"]["Medium"])
+            hard = str(jsonr["body"]["_medalsSinglePlayer"]["Hard"])
+            primary = str(jsonr["body"]["_medalsSinglePlayer"]["PrimaryOnly"])
+            deflation = str(jsonr["body"]["_medalsSinglePlayer"]["Deflation"])
+            military = str(jsonr["body"]["_medalsSinglePlayer"]["MilitaryOnly"])
+            apopalypse = str(jsonr["body"]["_medalsSinglePlayer"]["Apopalypse"])
+            reverse = str(jsonr["body"]["_medalsSinglePlayer"]["Reverse"])
+            magic = str(jsonr["body"]["_medalsSinglePlayer"]["MagicOnly"])
+            halfcash = str(jsonr["body"]["_medalsSinglePlayer"]["HalfCash"])
+            doublehp = str(jsonr["body"]["_medalsSinglePlayer"]["DoubleMoabHealth"])
+            abr = str(jsonr["body"]["_medalsSinglePlayer"]["AlternateBloonsRounds"])
+            impoppable = str(jsonr["body"]["_medalsSinglePlayer"]["Impoppable"])
+            chimpsblack = str(jsonr["body"]["_medalsSinglePlayer"]["CHIMPS-BLACK"])
+            chimpsred = str(jsonr["body"]["_medalsSinglePlayer"]["Clicks"] - jsonr["body"]["_medalsSinglePlayer"]["CHIMPS-BLACK"])
+            medals_embed = discord.Embed(color=self.color, title=f"{uname} [`/player`]")
+            medals_embed.set_thumbnail(url=avatar)
+            medals_embed.add_field(name="Easy", value=f"<:easy:1073876236507562024> {easy}", inline=False)
+            medals_embed.add_field(name="Primary Only", value=f"<:primary:1073876277263614063> {primary}", inline=False)
+            medals_embed.add_field(name="Deflation", value=f"<:deflation:1073876355307012106> {deflation}", inline=False)
+            medals_embed.add_field(name="Medium", value=f"<:medium:1073876475578699816> {medium}", inline=False)
+            medals_embed.add_field(name="Military Only", value=f"<:military:1073876516888399943> {military}", inline=False)
+            medals_embed.add_field(name="Reverse", value=f"<:reverse:1073876573003993179> {reverse}", inline=False)
+            medals_embed.add_field(name="Apopalypse", value=f"<:apopalypse:1073876616926724108> {apopalypse}", inline=False)
+            medals_embed.add_field(name="Hard", value=f"<:hard:1073872742153199646> {hard}", inline=False)
+            medals_embed.add_field(name="Magic Only", value=f"<:magic:1073872652114083961> {magic}", inline=False)
+            medals_embed.add_field(name="Alternate Bloons Rounds", value=f"<:abr:1073872586955567114> {abr}", inline=False)
+            medals_embed.add_field(name="Double HP MOABs", value=f"<:doublehp:1073872543150243931> {doublehp}", inline=False)
+            medals_embed.add_field(name="Half Cash", value=f"<:halfcash:1073872491560308837> {halfcash}", inline=False)
+            medals_embed.add_field(name="Impoppable", value=f"<:impoppable:1073876417349173248> {impoppable}", inline=False)
+            medals_embed.add_field(name="Red Chimps", value=f"<:redchimps:1073872376766398544> {chimpsred}", inline=False)
+            medals_embed.add_field(name="Black Chimps", value=f"<:blackchimps:1073872329916026930> {chimpsblack}", inline=False)
+            medals_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
+            self.options[0].default = False
+            self.options[1].default = False
+            self.options[2].default = True
+            await interaction.edit_original_response(embed=medals_embed, view=self.view)
 
 class PlayerView(discord.ui.View):
     def __init__(self, color, id):
@@ -102,6 +154,12 @@ class PlayerInfo(commands.Cog):
             veteran = str(jsonr["body"]["veteranRank"])
             most_experienced = jsonr["body"]["mostExperiencedMonkey"]
             most_experienced = nkapi.emotify(most_experienced)
+            herolist = [jsonr["body"]["heroesPlaced"]["AdmiralBrickell"], jsonr["body"]["heroesPlaced"]["Adora"], jsonr["body"]["heroesPlaced"]["Benjamin"], 
+                jsonr["body"]["heroesPlaced"]["Etienne"], jsonr["body"]["heroesPlaced"]["Geraldo"], jsonr["body"]["heroesPlaced"]["Gwendolin"], 
+                jsonr["body"]["heroesPlaced"]["ObynGreenfoot"], jsonr["body"]["heroesPlaced"]["PatFusty"], jsonr["body"]["heroesPlaced"]["Psi"], 
+                jsonr["body"]["heroesPlaced"]["Quincy"], jsonr["body"]["heroesPlaced"]["Sauda"], jsonr["body"]["heroesPlaced"]["StrikerJones"]]
+            most_used_hero = herolist.index(max(herolist))
+            most_used_hero = nkapi.herofy(most_used_hero)
             highest_round = str(jsonr["body"]["highestRound"])
             achievements = str(jsonr["body"]["achievements"])
             followers = str(jsonr["body"]["followers"])
@@ -113,6 +171,7 @@ class PlayerInfo(commands.Cog):
             player_embed.add_field(name="Rank", value=f"<:rank:1072789620883984414> {rank}", inline=False)
             player_embed.add_field(name="Veteran Rank", value=f"<:veteran:1072789996047704144> {veteran}", inline=False)
             player_embed.add_field(name="Most Experienced Monkey", value=most_experienced, inline=False)
+            player_embed.add_field(name="Most Used Hero", value=most_used_hero, inline=False)
             player_embed.add_field(name="Highest Round", value=f"<:red:1072864121088909322> {highest_round}", inline=False)
             player_embed.add_field(name="Followers", value=f"<:dartface:1072900168275468318> {followers}", inline=False)
             player_embed.add_field(name="Achievements", value=f"<:achievements:1073259163653787728> {achievements}", inline=False)
