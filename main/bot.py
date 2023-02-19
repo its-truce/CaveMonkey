70e84dc74edbd24171d13657f6fff4c2fbd37e50
itstruce <107202816+its-truce@users.noreply.github.com>
2023-02-19 18:01:58 +0530

    Update bot.py

--- main/bot.py
+++ main/bot.py
@@ -2,10 +2,11 @@
 import discord
 from discord.ext import commands
 from discord import app_commands
+from datetime import datetime
 
 # Setup
 token = "YOUR TOKEN HERE"
-initial_extensions = ("extensions.vtsg", "extensions.pop", "extensions.income", "extensions.player")
+initial_extensions = ("extensions.vtsg", "extensions.pop", "extensions.income", "extensions.player", "extensions.help")
 
 class Bot(commands.Bot):
     def __init__(self):
@@ -23,6 +24,7 @@ class Bot(commands.Bot):
             except Exception as e:
                 print(f"Failed to load extension {ext}.\nException:\n{e}")
 bot = Bot()
+bot.launch_time = datetime.utcnow()
 
 # Functionality
 @commands.is_owner()
@@ -30,20 +32,32 @@ bot = Bot()
 async def sync(ctx):
     await bot.tree.sync()
     embed = discord.Embed(color=0x5865f2, title="Sync [`-sync`]", description="Cave Monkey app commands have been synced successfully.")
-    embed.set_thumbnail(url="https://media.discordapp.net/attachments/1072801761775071252/1073502646666805248/pfp.png?width=580&height=580")
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/1074597800735690853/1074597847242117170/PicsArt_02-12-01.png")
     await ctx.send(embed=embed)
 
 @commands.is_owner()
 @bot.command()
-async def reload(ctx):
-    try:
-        for ext in initial_extensions:
-            await bot.reload_extension(ext)
-        embed = discord.Embed(color=0x5865f2, title="Reload [`-reload`]", description="Cave Monkey extensions have been reloaded successfully.")
-        embed.set_thumbnail(url="https://media.discordapp.net/attachments/1072801761775071252/1073502646666805248/pfp.png?width=580&height=580")
-        await ctx.send(embed=embed)
-    except Exception as e:
-        print(f"Failed to reload extension {ext}.\nException:\n{e}")
+async def reload(ctx, extarg: str = None):
+    if extarg is not None:
+        try:
+            await bot.reload_extension(f"extensions.{extarg}")
+            embed = discord.Embed(color=0x5865f2, title="Reload [`-reload`]", description=f"Reloaded the `{extarg}` extension successfully!")
+            embed.set_thumbnail(url="https://media.discordapp.net/attachments/1074597800735690853/1074597847242117170/PicsArt_02-12-01.png")
+            await ctx.send(embed=embed)
+        except Exception as e:
+            embed = discord.Embed(color=0x585f2, title="Encountered Problem [`-reload`]", description=f"```py\n{e}\n```")
+            await ctx.send(embed=embed)
+    else:
+        try:
+            for ext in initial_extensions:
+                await bot.reload_extension(ext)
+            embed = discord.Embed(color=0x5865f2, title="Reload [`-reload`]", description="Cave Monkey extensions have been reloaded successfully.")
+            embed.set_thumbnail(url="https://media.discordapp.net/attachments/1074597800735690853/1074597847242117170/PicsArt_02-12-01.png")
+            await ctx.send(embed=embed)
+        except Exception as e:
+            embed = discord.Embed(color=0x585f2, title="Encountered Problem [`-reload`]", description=f"```py\n{e}\n```")
+            embed.set_footer(text=f"ⓘ {ext} could not be loaded.")
+            await ctx.send(embed=embed)
 
 # Running
 bot.run(token)
