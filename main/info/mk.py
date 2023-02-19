c555a7be00827efebccc4d581894d915a7e1102e
itstruce <107202816+its-truce@users.noreply.github.com>
2023-02-19 18:01:01 +0530

    added monkey knowledge data

---
+++ main/info/mk.py
@@ -0,0 +1,61 @@
+dart = """Dart Monkey is affected by the following Monkey Knowledge points:
+```
+Increased Lifespan (longer projectile lifespan)
+Extra Dart Pops (+1 pierce)
+Crossbow Reach (increases range of xx3+ upgrades by 3)
+Force vs Force (4xx+ gets 2+ MOAB damage)
+4 And 4 (x3x gets 4 darts every 4th shot)
+Master Double Cross (lets you buy two xx5 darts)
+Come On Everybody! (primary towers attack 5 per cent faster if all are below tier 3, and cost 5 per cent less if all are tier 3 or 4)
+Bonus Monkey! (start game with a free dart monkey. only 1 free tower per game)
+```"""
+
+boomerang = """Boomerang Monkey is affected by the following Monkey Knowledge points:
+```
+Cheap Rangs (base cost reduced by 50)
+Extra Bounce (increases pierce of ricocheting glaives by 30)
+Recurring 'Rangs (returned boomerangs are thrown a second time)
+Hard Press (xx4+ special knockback boomerangs push back 30 per cent further.)
+Long Turbo (x4x+ ability lasts 15 seconds)
+Bionic Augmentation (has camo detection while x4x+ ability is active)
+```"""
+
+bomb = """Bomb Shooter is affected by the following Monkey Knowledge points:
+```
+Increased Lifespan (longer projectile lifespan)
+Fraggy Frags (frag bombs get +2 frags for xx2 and 302+)
+Budget Clusters (xx3 cost reduced by 100)
+Mega Mauler (increases MOAB damage of x3x attacks by 2)
+Violent Impact (4xx+ stuns for 25 per cent longer)
+Come On Everybody! (primary towers attack 5 per cent faster if all are below tier 3, and cost 5 per cent less if all are tier 3 or 4)
+```"""
+
+tack = """Tack Shooter is affected by the following Monkey Knowledge points:
+```
+Fast Tack Attacks (attack speed increased by 8 per cent)
+Increased Lifespan (longer projectile lifespan)
+Poppy Blades (x3x+ gets +2 pierce. also affects x4x+ abilities)
+Hard Tacks (base tower can pop frozen bloons)
+Big Inferno (increases range of 5xx by 3)
+Come On Everybody! (primary towers attack 5 per cent faster if all are below tier 3, and cost 5 per cent less if all are tier 3 or 4)
+```"""
+
+ice = """
+```
+Icy Chill (increases range by 3)
+So... Cold... (1xx slows by 10 per cent more, for a total of 60 per cent)
+Big Cryo Blast (increases blast radius of xx3 by 5)
+Hypothermia (x4x ability freezes bloons for +1 second)
+Come On Everybody! (primary towers attack 5 per cent faster if all are below tier 3, and cost 5 per cent less if all are tier 3 or 4)
+```"""
+
+glue = """
+```
+Increased Lifespan (longer projectile lifespan)
+Fast Glue (increased attack speed by 10 per cent)
+More Splatty Glue (increases pierce of x2x by 2)
+Aviation Grade Glue (increases MOAB slowdown of xx3+ by 12.5 per cent. slows down to 50 per cent speed now)
+Cheaper Solution (4xx cost reduced by 1000)
+Come On Everybody! (primary towers attack 5 per cent faster if all are below tier 3, and cost 5 per cent less if all are tier 3 or 4)
+Bonus Glue Gunner (start game with a free glue gunner. only 1 free tower per game)
+```"""
