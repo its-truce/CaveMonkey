1c048533117bee04f31de3525ef05d41d104146f
itstruce <107202816+its-truce@users.noreply.github.com>
2023-02-20 14:56:44 +0530

    Update income.py

--- main/extensions/income.py
+++ main/extensions/income.py
@@ -5,26 +5,15 @@ from typing import Literal
 import info.cash as cash
 import numpy as np
 
-class SaveUp(commands.Cog):
+class Income(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @app_commands.command(description="Find out which round you need to start saving up at for a certain amount of money.")
     @app_commands.describe(money="The amount of money you need.", round="The round you need the money by.", difficulty="The difficulty you're playing at.")
-    async def saveup(self, interaction: discord.Interaction, money: int, round: int, difficulty: Literal["easy/medium", "hard", "impoppable/chimps"] = "easy/medium"):
-        if round > 140:
-            error_embed = discord.Embed(color=0x5865f2, title="Encountered Problem [`/saveup`]", description="Round parameter cannot exceed 140.")
-            error_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
-            await interaction.response.send_message(embed=error_embed)
-        if round == 0:
-            error_embed = discord.Embed(color=0x5865f2, title="Encountered Problem [`/saveup`]", description="Round parameter cannot be 0.")
-            error_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
-            await interaction.response.send_message(embed=error_embed)
-        if money == 0:
-            error_embed = discord.Embed(color=0x5865f2, title="Encountered Problem [`/saveup`]", description="Money parameter cannot be 0.")
-            error_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
-            await interaction.response.send_message(embed=error_embed)                
-        elif difficulty == "easy/medium":
+    async def saveup(self, interaction: discord.Interaction, money: app_commands.Range[int, 1], round: app_commands.Range[int, 1, 140], 
+                difficulty: Literal["easy/medium", "hard", "impoppable/chimps"] = "easy/medium"):              
+        if difficulty == "easy/medium":
             if money > cash.medium[round - 1]:
                 error_embed = discord.Embed(color=0x5865f2, title="Encountered Problem [`/saveup`]", description="Money parameter cannot exceed maximum cash earned by round.")
                 error_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
@@ -73,5 +62,56 @@ class SaveUp(commands.Cog):
                 save_embed.set_footer(text="ⓘ does not factor in external farming or any money you already have")
                 await interaction.response.send_message(embed=save_embed)
 
+    @app_commands.command(description="Find out which round you'll get a certain amount of money by.")
+    @app_commands.describe(money="The amount of money you need.", round="The current round.", difficulty="The difficulty you're playing at.")
+    async def saveby(self, interaction: discord.Interaction, money: app_commands.Range[int, 1], round: app_commands.Range[int, 1, 140], 
+                difficulty: Literal["easy/medium", "hard", "impoppable/chimps"] = "easy/medium"):
+            if difficulty == "easy/medium":
+                try:
+                    cash_total = cash.medium[round] + money
+                    medium_array = np.array(cash.medium)
+                    mask = (medium_array >= cash_total)
+                    final = np.flatnonzero(mask)[0]
+                    save_embed = discord.Embed(color=0xffd04f, title="Result [`/saveby`]", description=f"If you start saving up at {round}, you will get {money} by **round {final}**.\n\nExpression:\n> *total = money obtained by the end of round + money needed*\n> *round = minimum round to the required amount*")
+                    save_embed.set_thumbnail(url="https://static.wikia.nocookie.net/b__/images/2/23/Money_icon.png/revision/latest?cb=20210727151424&path-prefix=bloons")
+                    save_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
+                    save_embed.set_footer(text="ⓘ does not factor in external farming or any money you already have")
+                    await interaction.response.send_message(embed=save_embed)
+                except IndexError as e:
+                    error_embed = discord.Embed(color=0x5865f2, title="Encountered Problem [`/saveby`]", description="Money parameter is not earned by round 140 or less. Please lower the value.")
+                    error_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
+                    await interaction.response.send_message(embed=error_embed)
+            elif difficulty == "hard":
+                try:
+                    cash_total = cash.hard[round] + money
+                    hard_array = np.array(cash.hard)
+                    mask = (hard_array >= cash_total)
+                    final = np.flatnonzero(mask)[0]
+                    save_embed = discord.Embed(color=0xffd04f, title="Result [`/saveby`]", description=f"If you start saving up at {round}, you will get {money} by **round {final}**.\n\nExpression:\n> *total = money obtained by the end of round + money needed*\n> *round = minimum round to the required amount*")
+                    save_embed.set_thumbnail(url="https://static.wikia.nocookie.net/b__/images/2/23/Money_icon.png/revision/latest?cb=20210727151424&path-prefix=bloons")
+                    save_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
+                    save_embed.set_footer(text="ⓘ does not factor in external farming or any money you already have")
+                    await interaction.response.send_message(embed=save_embed)
+                except IndexError as e:
+                    error_embed = discord.Embed(color=0x5865f2, title="Encountered Problem [`/saveby`]", description="Money parameter is not earned by round 140 or less. Please lower the value.")
+                    error_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
+                    await interaction.response.send_message(embed=error_embed)
+            elif difficulty == "impoppable/chimps":
+                try:
+                    cash_total = cash.impoppable[round] + money
+                    impoppable_array = np.array(cash.impoppable)
+                    mask = (impoppable_array >= cash_total)
+                    final = np.flatnonzero(mask)[0]
+                    save_embed = discord.Embed(color=0xffd04f, title="Result [`/saveby`]", description=f"If you start saving up at {round}, you will get {money} by **round {final}**.\n\nExpression:\n> *total = money obtained by the end of round + money needed*\n> *round = minimum round to the required amount*")
+                    save_embed.set_thumbnail(url="https://static.wikia.nocookie.net/b__/images/2/23/Money_icon.png/revision/latest?cb=20210727151424&path-prefix=bloons")
+                    save_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
+                    save_embed.set_footer(text="ⓘ does not factor in external farming or any money you already have")
+                    await interaction.response.send_message(embed=save_embed)
+                except IndexError as e:
+                    error_embed = discord.Embed(color=0x5865f2, title="Encountered Problem [`/saveby`]", description="Money parameter is not earned by round 140 or less. Please lower the value.")
+                    error_embed.set_author(name=interaction.user.display_name, icon_url=interaction.user.display_avatar.url)
+                    await interaction.response.send_message(embed=error_embed)
+
+
 async def setup(bot):
-    await bot.add_cog(SaveUp(bot))
+    await bot.add_cog(Income(bot))
