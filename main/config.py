d886acf1c3c80452974198d346a1a2da62a22304
itstruce <107202816+its-truce@users.noreply.github.com>
2023-02-23 20:04:48 +0530

    Update config.py

--- main/config.py
+++ main/config.py
@@ -1,2 +1,3 @@
-# Enter your bot's token here:
-TOKEN = "YOUR TOKEN HERE"
+token = "YOUR TOKEN HERE" # your bot's token
+prefix = "-" # recommended not to change
+owner_id = 626333424965386240 # your id
