a65465f68fd1032df04414c79e94fac70ad4c31d
itstruce <107202816+its-truce@users.noreply.github.com>
2023-02-24 20:28:55 +0530

    Update mk.py

--- main/info/mk.py
+++ main/info/mk.py
@@ -40,7 +40,7 @@ Big Inferno (increases range of 5xx by 3)
 Come On Everybody! (primary towers attack 5 per cent faster if all are below tier 3, and cost 5 per cent less if all are tier 3 or 4)
 ```"""
 
-ice = """
+ice = """Ice Monkey is affected by the following Monkey Knowledge points:
 ```
 Icy Chill (increases range by 3)
 So... Cold... (1xx slows by 10 per cent more, for a total of 60 per cent)
@@ -49,7 +49,7 @@ Hypothermia (x4x ability freezes bloons for +1 second)
 Come On Everybody! (primary towers attack 5 per cent faster if all are below tier 3, and cost 5 per cent less if all are tier 3 or 4)
 ```"""
 
-glue = """
+glue = """Glue Gunner is affected by the following Monkey Knowledge points:
 ```
 Increased Lifespan (longer projectile lifespan)
 Fast Glue (increased attack speed by 10 per cent)
@@ -58,4 +58,102 @@ Aviation Grade Glue (increases MOAB slowdown of xx3+ by 12.5 per cent. slows dow
 Cheaper Solution (4xx cost reduced by 1000)
 Come On Everybody! (primary towers attack 5 per cent faster if all are below tier 3, and cost 5 per cent less if all are tier 3 or 4)
 Bonus Glue Gunner (start game with a free glue gunner. only 1 free tower per game)
+```"""
+
+sniper = """Sniper Monkey is affected by the following Monkey Knowledge points:
+```
+Elite Military Training (+1000 xp and earn xp 5 per cent faster)
+Ceramic Shock (all attacks slow down cerams by 90 per cent for 1 second)
+Cheaper Maiming (4xx cost reduced by 1000)
+Flanking Maneuvers (increases attack speed by 10 per cent when set to last)
+Master Defender (xx5 lives lost buff has no cooldown)
+Military Conscription (purchase price of first military monkey is two thirds the normal)
+Advanced Logistics (base cost reduced by 5 per cent)
+```"""
+
+sub = """Monkey Sub is affected by the following Monkey Knowledge points:
+```
+Naval Upgrades (increases default dart pierce by 1)
+Elite Military Training (+1000 xp and earn xp 5 per cent faster)
+Breaking Ballistic (increases ceramic damage of x3x+ by 1)
+Quad Burst (xx2+ darts split into 4 instead of 3)
+Flanking Maneuvers (increases attack speed by 10 per cent when set to last)
+Sub Admiral (xx5 buffs apply to all monkeys)
+Military Conscription (purchase price of first military monkey is two thirds the normal)
+Advanced Logistics (base cost reduced by 5 per cent)
+```"""
+
+bucc = """Monkey Buccaneer is affected by the following Monkey Knowledge points:
+```
+Naval Upgrades (increases default dart pierce by 1)
+Elite Military Training (+1000 xp and earn xp 5 per cent faster)
+Big Bunch (increases number of grapes for x1x+ by 1)
+Faster Takedowns (decreases x4x+ ability cooldown by 5 seconds)
+Trade Agreements (increases end of round bonus of xx3+ by 20)
+Flanking Maneuvers (increases attack speed by 10 per cent when set to last)
+Military Conscription (purchase price of first military monkey is two thirds the normal)
+Advanced Logistics (base cost reduced by 5 per cent)
+```"""
+
+ace = """Monkey Ace is affected by the following Monkey Knowledge points:
+```
+Airforce Upgrades (increases pierce by 1)
+Elite Military Training (+1000 xp and earn xp 5 per cent faster)
+Accelerated Darts (darts fly 50 per cent faster)
+Targeted Pineapples (pineapples are dropped near tracks)
+Gun Coolant (increases attack speed by 10 per cent)
+Wingmonkey (flight pathing that coordinates multiple aces on staggered attack runs)
+Aeronautic Subsidy (tier 5 upgrades cost 10 per cent less)
+Military Conscription (purchase price of first military monkey is two thirds the normal)
+Advanced Logistics (base cost reduced by 5 per cent)
+```"""
+
+heli = """Heli Pilot is affected by the following Monkey Knowledge points:
+```
+Airforce Upgrades (increases pierce by 1)
+Elite Military Training (+1000 xp and earn xp 5 per cent faster)
+Rapid Razors (rotors pop 25 per cent faster)
+Charged Chinooks (x4x+ ability gives 25 per cent more lives and cash)
+Military Conscription (purchase price of first military monkey is two thirds the normal)
+Door Gunner (x5x allows tier 4 and below towers to attack while in transit)
+Advanced Logistics (base cost reduced by 5 per cent)
+```"""
+
+mortar = """Mortar Monkey is affected by the following Monkey Knowledge points:
+```
+Elite Military Training (+1000 xp and earn xp 5 per cent faster)
+Extra Burny Stuff (xx2+ napalm burns 0.25 seconds faster)
+Paint Stripper (lets xx4 remove fortification from DDTs)
+Budget Battery (x4x base cost reduced by 600)
+Military Conscription (purchase price of first military monkey is two thirds the normal)
+Advanced Logistics (base cost reduced by 5 per cent)
+```"""
+
+dartling = """Mortar Monkey is affected by the following Monkey Knowledge points:
+```
+Elite Military Training (+1000 xp and earn xp 5 per cent faster)
+Emergency Unlock (when locked gains an ability that unlocks all dartling gunners and spin faster by 33 per cent for 5 seconds)
+Gorgon Storm (x4x+ ability stuns bloons for 0.5 seconds and MOABs for 0.1 seconds))
+Military Conscription (purchase price of first military monkey is two thirds the normal)
+Advanced Logistics (base cost reduced by 5 per cent)
+```"""
+
+wizard = """Wizard Monkey is affected by the following Monkey Knowledge points:
+```
+Lingering Magic (longer projectile lifespan)
+Magic Tricks (1xx and xx1 cost base cost reduced bt 25)
+Hot Magic (can pop frozen bloons)
+Flame Jet (x3x+ projectiles fly 50 per cent faster)
+Arcane Impale (increase MOAB and ceram damage of 4xx+ by 1)
+```"""
+
+super = """Super Monkey is affected by the following Monkey Knowledge points:
+```
+SUPER Range (increase range of x1x+ by 10 per cent)
+Lingering Magic (longer projectile lifespan)
+Hot Magic (can pop frozen bloons)
+Heavy Knockback (increases knockback of xx1+ by 5 per cent)
+Strike Down The False (lets 3xx pop purple bloons)
+X-Ray Ultra (lets xx2+ see through line of sight blockers)
+There Can Only Be One (lets you get the VTSG)
 ```"""
