bf9fd5c2b95aa7442730abb7849132ad247f9634
itstruce <107202816+its-truce@users.noreply.github.com>
2023-02-24 20:28:09 +0530

    Update config.py

--- main/config.py
+++ main/config.py
@@ -1,3 +1,5 @@
 token = "YOUR TOKEN HERE" # your bot's token
 prefix = "-" # recommended not to change
 owner_id = 626333424965386240 # your id
+initial_extensions = ("extensions.vtsg", "extensions.pop", "extensions.income", "extensions.player", 
+                      "extensions.help", "extensions.knowledge", "extensions.dev") # your extensions
